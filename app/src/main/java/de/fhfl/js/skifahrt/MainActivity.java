2845f85cf1480dc486746986b7f95c67d0d91cc8
Jasmin <minestern@web.de>
2015-11-17 14:20:28 +0100

    Aufbau der Activitys für die einzelnen Levels

--- app/src/main/java/de/fhfl/js/skifahrt/MainActivity.java
+++ app/src/main/java/de/fhfl/js/skifahrt/MainActivity.java
@@ -1,7 +1,14 @@
 package de.fhfl.js.skifahrt;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import de.fhfl.js.skifahrt.level.LevelFourActivity;
+import de.fhfl.js.skifahrt.level.LevelOneActivity;
+import de.fhfl.js.skifahrt.level.LevelThreeActivity;
+import de.fhfl.js.skifahrt.level.LevelTwoActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openLevelOne(View view) {
+        Intent intent = new Intent(this, LevelOneActivity.class);
+    }
+
+    public void openLevelTwo(View view) {
+        Intent intent = new Intent(this, LevelTwoActivity.class);
+
+    }
+
+    public void openLevelThree(View view) {
+        Intent intent = new Intent(this, LevelThreeActivity.class);
+    }
+
+    public void openLevelFour(View view) {
+        Intent intent = new Intent(this, LevelFourActivity.class);
     }
 }
---
+++ app/src/main/java/de/fhfl/js/skifahrt/level/LevelFourActivity.java
@@ -0,0 +1,21 @@
+package de.fhfl.js.skifahrt.level;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import de.fhfl.js.skifahrt.R;
+
+/**
+ * Created by Jasmin on 17.11.2015.
+ */
+public class LevelFourActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
